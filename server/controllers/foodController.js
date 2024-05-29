f31ff2b77e1c20a73007522b9a96ef4da85ec96c
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-05-29 15:07:28 +0530

    add all food list

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -27,7 +27,13 @@ const addFood = async (req, res) => {
 
 // all food list
 const listFood = async (req,res) => {
-
+      try {
+        const foods = await foodModel.find({});
+        res.json({success:true, data:foods})
+      } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error"})
+      }
 }
 
 export {addFood,listFood}
