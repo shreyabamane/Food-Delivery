5c87cddbf48761e31b7ebbf83f1c250fc2bb2355
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-06-04 19:31:09 +0530

    order status change after clicking track order button

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async (req, res) => {
 
     //client url
-    const client_url = "http://localhost:5173"
+    const client_url = "http://localhost:5174";
 
     try {
         const newOrder = new orderModel({
