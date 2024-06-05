710d0c6c69eec35ae2fbb8a030b23a76ae451b6a
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-06-05 21:09:09 +0530

    modify

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,5 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_CONNECTION,
-        {useNewUrlParser: true, useUnifiedTopology: true,}).then(()=>console.log("MongoDB Connected"));
+    await mongoose.connect(process.env.MONGO_CONNECTION).then(()=>console.log("MongoDB Connected"));
 }
